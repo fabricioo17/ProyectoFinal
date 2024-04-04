78d2320f51723874420c3df89f9bfb58e67f2857
fabricioo <josefabricio172004@gmail.com>
2024-04-04 12:11:03 +0200

    añadiendo movimiento caballo

--- src/Piezas/Tipos/Caballo.java
+++ src/Piezas/Tipos/Caballo.java
@@ -4,6 +4,16 @@ import Piezas.Jugadores;
 import Piezas.Pieza;
 
 public class Caballo extends Pieza {
+    private   int limitederecha1=0;
+
+    private   int limitederecha2=0;
+    private int limiteArriba1 =0;
+    private  int limititeArriba2=0;
+    private    int limiteAbajo1=0;
+
+    private    int limiteAbajo2=0;
+    protected   int limiteIzquierda=0;
+    protected   int limiteIzquierda2=0;
     public Caballo(Jugadores propietario) {
         super(propietario);
     }
@@ -12,8 +22,26 @@ public class Caballo extends Pieza {
 
     public void espacioDisponible(Pieza[][] table){
 
-        
+        // arriba derecha lejos
+        if (table[posicionX-2][posicionY+1]instanceof Bloqueo){
+            table[posicionX-2][posicionY+1]=null;
+        }
 
+        //arriba izquierda
+
+        if (table[posicionX-2][posicionY-1] instanceof Bloqueo ){
+            table[posicionX-2][posicionY-1]=null;
+        }
+
+        //abajo derecha
+        if (table[posicionX+2][posicionY+1] instanceof Bloqueo){
+            table[posicionX+2][posicionY+1]=null;
+        }
+
+        //abajo izquierda
+        if (table[posicionX+2][posicionY-1] instanceof  Bloqueo){
+            table[posicionX+2][posicionY-1]=null;
+        }
 
     }
 }
